b550842054876e75f386051b20948757a21b3ef5
danladi619 <danladifahad96@gmail.com>
2019-11-27 00:27:42 +0100

    Added video 3

---
+++ week1/video_3_strings.py
@@ -0,0 +1,9 @@
+print("i don't think she's 18")
+print('she said "what part of the cow is the meatloaf from"')
+print('i don\'t think shes 18')
+print(r'C:\Users\danla\Downloads\nightsleep')
+firstname = 'fahad '
+lastname = 'danladi'
+name = firstname + lastname
+
+print(name)
