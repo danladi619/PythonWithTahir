ef83c2a71bd3b03b6872590c3e94e891bddb2e4f
danladi619 <danladifahad96@gmail.com>
2019-12-11 16:29:47 +0100

    Exercise 2.9

---
+++ week2/exercise2.9.py
@@ -0,0 +1,14 @@
+tuple_list = (12,24,36,48,60,72)
+total = 0
+for tuple_lis in tuple_list:
+    total += tuple_lis
+
+length = len(tuple_list)
+average = float(total/length)
+print('The average is: ' + str(average))
+
+
+
+
+
+
