84372dbb74020f37687dc18e183d184d9aaea4aa
danladi619 <danladifahad96@gmail.com>
2020-01-06 12:28:30 +0100

    max2_min2 function

---
+++ week2/max2_min2.py
@@ -0,0 +1,16 @@
+def max2(x,y):
+    if x > y:
+        print(f"{x} is the maximum")
+    else:
+        print(f"{y} is the maximum")
+
+def min2(x,y):
+    if x < y:
+        print(f"{x} is the minimum")
+    else:
+        print(f"{y} is the minimum")
+
+max2(7,9)
+min2(23,10)
+
+
